f306d3dc8203d42ca26328a7640370590afaa88a
Alexander KIRILOV <alexander.n.kirilov@gmail.com>
2019-10-19 11:29:07 +0200

    Added dirname

--- runner.sh
+++ runner.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-mkdir -p logs
+dirname="$( cd "$( echo "${BASH_SOURCE[0]%/*}" )"; pwd )"
 
-/usr/bin/node ./src/index-leboncoin.js >> ./logs/leboncoin-bot.log 2>&1
-/usr/bin/node ./src/index-seloger.js >> ./logs/seloger-bot.log 2>&1
+mkdir -p $dirname/logs
+
+/usr/bin/node $dirname/src/index-leboncoin.js >> $dirname/logs/leboncoin-bot.log 2>&1
+/usr/bin/node $dirname/src/index-seloger.js >> $dirname/logs/seloger-bot.log 2>&1
 
